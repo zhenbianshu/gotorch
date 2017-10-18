1f5053166f0219492eda3cb87ce72a9b21c8af63
zhenbianshu <zhenbianshu@foxmail.com>
2017-10-18 19:58:26 +0800

    解析当前时间戳；

--- src/task/task.go
+++ src/task/task.go
@@ -16,19 +16,33 @@ type timeRange struct {
 	every int
 }
 
+var timeOrder = []string{"month", "week", "day", "hour", "minute", "second"}
+
 func (t *taskItem) init() {
 	t.setNext()
 }
 
 func (t *taskItem) setNext() {
-	// todo method to get next time
+	currentTime := make(map[string]int)
+	current := time.Now()
+	currentTime["month"] = int(current.Month())
+	currentTime["week"] = int(current.Weekday())
+	currentTime["day"] = int(current.Day())
+	currentTime["hour"] = int(current.Hour())
+	currentTime["minute"] = int(current.Minute())
+	currentTime["second"] = int(current.Second())
+
+	for _, order := range timeOrder {
+		for _, rangeItem := range t.times[order] {
+			rangeItem.start++
+		}
+	}
 }
 
 func (t *taskItem) CheckTime() {
 	current := time.Now()
 	timestamp := current.Unix()
-
-	if timestamp > t.next {
+	if timestamp >= t.next {
 		t.Exec()
 		t.setNext()
 	}
