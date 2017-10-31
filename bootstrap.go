c4be46b55d2d51678669ee684195d88f4b1d67dc
zhenbianshu <zhenbianshu@foxmail.com>
2017-10-31 21:26:30 +0800

    添加命令行参数处理；

--- bootstrap.go
+++ bootstrap.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"task"
 	"time"
 )
 
+const Version = "0.9"
+
 func main() {
+	dealCliArgs()
 	task.Init()
 	for {
 		task.Run()
 		time.Sleep(time.Millisecond * 200)
 	}
+}
+
+func dealCliArgs() {
+	bootType := os.Args[1]
+	if bootType == "-s" || bootType == "--start" {
+		return
+	} else if bootType == "-r" || bootType == "--restart" {
+		// task.Reload()
+	} else if bootType == "-e" || bootType == "--end" {
+		// task.End()
+	} else if bootType == "-v" || bootType == "--version" {
+		fmt.Println("CopyRight @zhenbianshu V" + Version)
+	} else if bootType == "-h" || bootType == "--help" {
+		fmt.Println("-s --start 启动服务")
+		fmt.Println("-e --end 关闭服务")
+		fmt.Println("-r --restart 平滑重启服务")
+		fmt.Println("-v --version 查看服务版本")
+		fmt.Println("-h --help 查看帮助")
+	} else {
+		fmt.Println("unknown parameter, use -h or --help to get help!")
+	}
+	os.Exit(0)
 }
