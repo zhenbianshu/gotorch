514cb4bc87fa8b7ad933c586db08015ab1db9ca2
zhenbianshu <zhenbianshu@foxmail.com>
2017-11-14 19:53:15 +0800

    使用goterm打印单次任务状态；

--- src/stat/main.go
+++ src/stat/main.go
@@ -1,41 +1,29 @@
 package stat
 
 import (
-	"fmt"
 	"time"
+	tm "goterm"
 )
 
 func ShowStat() {
-	// 最好能依附到标准输出
-	for {
-		clear()
-		printStat()
-		time.Sleep(1 * time.Second)
-	}
-}
-
-/**
- *清屏
- */
-func clear() {
-	for i := 0; i < 100; i++ {
-		fmt.Printf("\n")
-	}
-	fmt.Printf("\033[0;0H")
+	tm.Clear()
+	tm.MoveCursor(1, 1)
+	tm.Println("Current Time:", time.Now().Format(time.RFC1123))
+	printStat()
+	tm.Flush()
 }
 
 func printStat() {
 	stats := collectStat()
 	for _, stat := range stats {
-		fmt.Println(stat)
+		tm.Println(stat)
 	}
-	fmt.Printf("\033[0;0H")
 }
 
 func collectStat() (stats []string) {
+	taskStat := getTaskStat()
 	cpuStat := getCpuStat()
 	memStat := getMemStat()
-	taskStat := getTaskStat()
 
 	stats = []string{cpuStat, memStat, taskStat}
 	return stats
